7e7805a4588884f756856554816fda5dd6fde506
WUSTBlank <hehaowei@126.com>
2020-09-25 19:46:02 +0800

    first commit

---
+++ src/main/java/com/hhw/layuitest/layuitest/LayuitestApplication.java
@@ -0,0 +1,13 @@
+package com.hhw.layuitest.layuitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayuitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayuitestApplication.class, args);
+    }
+
+}
